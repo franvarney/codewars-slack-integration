0e7ccbdd77fed8cd20fce73ffd77f235c1dbf197
Fran Varney <fran@modulus.io>
2016-03-09 08:48:53 -0500

    Add server connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const Hapi = require('hapi')
 const Logger = require('@modulus/logger')('index')
+const Mongoose = require('mongoose')
 
 const Auth = require('./handlers/auth')
 const Config = require('../config')
 
 var server = new Hapi.Server()
+
+Mongoose.connect(Config.mongo.url, (err) => {
+  if (err) {
+    Logger.error(`Mongoose.connect error: ${err.message}`)
+    throw err
+  }
+
+  Logger.info(`Connected to ${Config.mongo.url}`)
+})
 
 server.connection({
   host: process.env.NODE_ENV === 'production' ? null : Config.host,

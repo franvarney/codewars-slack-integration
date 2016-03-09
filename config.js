18dbdb98f5a2090fc7346c659682170b813a793f
Fran Varney <fran@modulus.io>
2016-03-09 08:48:44 -0500

    Add mongo url option

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
   host: process.env.HOST || 'localhost',
+  mongo: {
+    url: process.env.MONGO_URL || 'mongodb://localhost:27017/CodeWars'
+  },
   port: process.env.PORT || '8080',
   slack: {
     token: process.env.SLACK_TOKEN || 'token'

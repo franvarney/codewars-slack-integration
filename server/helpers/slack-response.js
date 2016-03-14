885a56ca751988e09ca2673861e23bc6e77e503d
Fran Varney <fran@modulus.io>
2016-03-14 16:00:00 +0100

    Add getQuestion method

--- server/helpers/slack-response.js
+++ server/helpers/slack-response.js
@@ -1,3 +1,40 @@
+const Config = require('../../config')
+
+function formatQuestion(text) {
+  var out = {
+    fallback: text.description,
+    text: text.description,
+    title: text.name,
+    fields: [
+      {
+        title: 'Rank',
+        value: `${Math.abs(text.rank)} kyu`,
+        short: true
+      },
+      {
+        title: 'Tags',
+        value: text.tags.slice(0, 2).toString().replace(',', ', '),
+        short: true
+      }
+    ],
+    mrkdwn_in: ['text', 'fallback']
+  }
+
+  if (Config.codewars.submissions) {
+    out.fields.push({
+      title: 'Make an attempt (at least once)',
+      value: '`/codewars attempt <' + text.slug + '> <(your code wrapped in parentheses>`'
+    })
+
+    out.fields.push({
+      title: 'Submit (after attempt)',
+        value: '`/codewars submit <' + text.slug + '> <(your code wrapped in parentheses>`'
+    })
+  }
+
+  return out
+}
+
 /*
  * Build a user only response.
  * @param {string} text - The text to respond with.
@@ -7,5 +44,21 @@ exports.ephemeral = function (text) {
   return {
     response_type: 'ephemeral',
     text: text
+  }
+}
+
+/*
+ * Build a channel only response.
+ * @return {object} body - The question from codewars.com.
+*/
+exports.channel = function (text) {
+  text = JSON.parse(text)
+
+  if (text && text.description) text = formatQuestion(text)
+
+  return {
+    response_type: 'in_channel',
+    text: 'Codewars.com Question:',
+    attachments: [text]
   }
 }
---
+++ server/library/bot.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const Request = require('request')
+
+const Config = require('../../config')
+const User = require('./user')
+
+class Bot {
+  constructor() {}
+
+  /*
+   * Get a new question from codewars.com.
+   * @param {object} data - The payload sent from Slack.
+   * @return {boolean} body - The question from codewars.com.
+  */
+  getQuestion(data, done) {
+    console.log(data)
+    var query = {
+      'team.name': data.team_domain,
+      'user.name': data.user_name
+    }
+
+    User().getToken(query, (err, token) => {
+      if (err) return done(err)
+
+      var apiUrl = 'https://www.codewars.com/api/v1'
+      var options = {
+        method: 'POST',
+        url: `${apiUrl}/code-challenges/${Config.codewars.language}/train`,
+        headers: {
+          Authorization: Config.codewars.token || token
+        },
+        qs: {
+          strategy: 'random'
+        }
+      }
+
+      Request(options, (err, response, body) => {
+        if (err) return done(err)
+        done(null, body)
+      })
+    })
+  }
+}
+
+module.exports = function () {
+  return new Bot()
+}

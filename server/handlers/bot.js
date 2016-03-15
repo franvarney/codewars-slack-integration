f476b9baca2b51c492a8ee11bf2830cc3c519de7
Fran Varney <fran@modulus.io>
2016-03-15 16:00:00 +0100

    Add question handler

--- server/handlers/bot.js
+++ server/handlers/bot.js
@@ -4,7 +4,7 @@ const Command = require('../library/command')
 const Response = require('../helpers/slack-response')
 
 module.exports = function (request, reply) {
-  Logger.debug(`Incoming data ${JSON.stringify(request.payload)}`)
+  Logger.debug(`Incoming data ${request.payload}`)
 
   var command = request.payload.text.split(' ')
 
@@ -17,6 +17,16 @@ module.exports = function (request, reply) {
         }
 
         return reply(Response.ephemeral('Successfully set token!'))
+      })
+      break;
+    case 'question':
+      Command().question(request.payload, (err, question) => {
+        if (err) {
+          Logger.error(`commmands.question ${err.message}`)
+          return reply(Response.ephemeral(err.message))
+        }
+
+        return reply(Response.channel(question))
       })
       break;
   }
